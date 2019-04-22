81e8c9dcbf730b6d0a9f3b39401b663a847dfbae
azarrias <adolfo.zarrias@gmail.com>
2019-04-22 13:04:14 +0200

    Add the push library to render graphics in a retro aesthetic

--- main.lua
+++ main.lua
@@ -1,22 +1,47 @@
+--[[ push is a library that allow us to draw our game at a virtual resolution, instead of however large our
+     window is; used to provide a more retro aesthetic
+     https://github.com/Ulydev/push
+]]
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 FONT_SIZE = 12
 
 -- Runs when the game first starts up, only once; used to initialize the game
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-        vsync = true
-    })
+  -- use nearest-neighbor (point) filtering on upscaling and downscaling to prevent blurring of text and 
+  -- graphics instead of the bilinear filter that is applied by default 
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  -- initialize virtual resolution, which will be rendered within the actual window no matter its
+  -- dimensions; wraps the love.window.setMode call
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+  })
+end
+
+-- Keyboard handling, called by LOVE2D for each frame
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  end
 end
 
 -- Called after update by LOVE2D, used to draw anything to the screen, updated or otherwise
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong!',                      -- text to render
-        0,                                  -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - FONT_SIZE / 2,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,                       -- number of pixels to center within (the entire screen here)
-        'center')                           -- alignment mode, can be 'center', 'left', or 'right'
+  -- begin rendering at virtual resolution (push works as a state machine, like OpenGL)
+  push:apply('start')
+  
+  love.graphics.printf('Hello Pong!',  
+    0,                                  -- starting X (0 since we're going to center it based on width)
+    VIRTUAL_HEIGHT / 2 - FONT_SIZE / 2, -- starting Y (halfway down the screen)
+    VIRTUAL_WIDTH,                      -- number of pixels to center within (the entire screen here)
+    'center')                           -- alignment mode, can be 'center', 'left', or 'right'
+  
+  -- end rendering at virtual resolution
+  push:apply('end')
 end
