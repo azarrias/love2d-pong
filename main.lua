d5828123a7f3e246d74302615a64bd58dec0c078
azarrias <adolfo.zarrias@gmail.com>
2019-04-22 11:54:04 +0200

    Create hello world kind of lua project using the LOVE2D interpreter

---
+++ main.lua
@@ -0,0 +1,22 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720 
+FONT_SIZE = 12
+
+-- Runs when the game first starts up, only once; used to initialize the game
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+-- Called after update by LOVE2D, used to draw anything to the screen, updated or otherwise
+function love.draw()
+    love.graphics.printf(
+        'Hello Pong!',                      -- text to render
+        0,                                  -- starting X (0 since we're going to center it based on width)
+        WINDOW_HEIGHT / 2 - FONT_SIZE / 2,  -- starting Y (halfway down the screen)
+        WINDOW_WIDTH,                       -- number of pixels to center within (the entire screen here)
+        'center')                           -- alignment mode, can be 'center', 'left', or 'right'
+end
